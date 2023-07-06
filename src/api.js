1febf6d84e7be41ae5cdbb27b08ed66f3103d0d8
srilalithasurampudi <38986445+srilalithasurampudi@users.noreply.github.com>
2023-07-06 12:00:46 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a8cb4a9ac3mshdabf626cd244ffep1a0121jsndfab7926ec6f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e855fd3c118cc72a62aa4e4ae753ac8b";
